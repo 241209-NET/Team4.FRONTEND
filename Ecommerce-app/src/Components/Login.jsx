6716c6f97652e7ba6fbf0b2b4d480f8ed8f7fe4a
Ethan Catania <101900207+ethancatania@users.noreply.github.com>
2025-01-16 16:11:18 -0500

    Styled and added functionality to Login page
    Updated css and Login page correctly communicates with databse.

--- Ecommerce-app/src/Components/Login.jsx
+++ Ecommerce-app/src/Components/Login.jsx
@@ -2,51 +2,139 @@ import * as React from 'react';
 import { AppProvider } from '@toolpad/core/AppProvider';
 import { SignInPage } from '@toolpad/core/SignInPage';
 import { useTheme } from '@mui/material/styles';
-
+import {Button} from '@mui/material';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 // preview-start
 const providers = [{ id: 'credentials', name: 'Email and Password' }];
 // preview-end
 
-const signIn = async (provider, formData) => {
-  const promise = new Promise((resolve) => {
-    setTimeout(() => {
-      // need to add the sign-in handling where the alert is
-      if (LoginAPI(formData.get('email'), formData.get('password')))
-      {
-      alert(
-        `Signing in with "${provider.name}" and credentials: ${formData.get('email')}, ${formData.get('password')}`,
-      );
+const SignIn = async (_, formData) => {
+
+  try{
+    let email = await formData.get("email");
+    let password = await formData.get("password");
+      if(!LoginAPI(email, password)){
+        alert('Either Password or Email is Empty!');
+        return;
       }
-      else
-      {
-        alert(
-          `Invalid username or password`,
-        );
+      const response = await axios.get(`http://localhost:5231/User/username/${email}`);
+
+      if(!response.data){
+        alert('User not found'); 
+        return;
       }
-      resolve();
-    }, 300);
-  });
-  return promise;
+
+      if(response.data.password != password){
+        alert('Incorrect Password'); 
+        return;
+      }
+
+      if(response.data){
+        //need to nav to another page
+      } else {
+          alert("SomeError")
+      }
+    } catch(TypeError){}
+}
+
+const NewAccount = async ( _, formData) => {
+
+  try{
+    let email =  await formData.get("email");
+    let password = await formData.get("password");
+      if(!RegisterAPI(email,password)){
+        alert('Either Password or Email is Empty!');
+        return;
+      }
+
+      const response = await axios.post('http://localhost:5231/User', {
+          username: email,
+          password: password
+      });
+
+      if(response.data){
+          alert(`Account with username ${email} successfully created`);
+      } else {
+          alert("SOME ERROR");
+      }
+  }   catch(TypeError){}
 };
 
 export default function CredentialsSignInPage() {
   const theme = useTheme();
+
   return (
-    // preview-start
-    <AppProvider theme={theme}>
-      <SignInPage
-        signIn={signIn}
-        providers={providers}
-        slotProps={{ emailField: { autoFocus: false } }}
-      />
-    </AppProvider>
-    // preview-end
+      <AppProvider theme={theme}>
+      <div id="LoginPage">
+          <SignInPage
+              signIn={SignIn}
+              providers={providers}
+              slots={{
+                title: TitleLogin,
+                subtitle: Subtitle,
+                submitButton: Login,
+                rememberMe: Subtitle
+              }}
+          />
+        <div id='greeting'>
+          <h3>Welcome to Our E-Commerce Site!</h3>
+          <img src="/dollar.png" alt="Ecommerce Logo" style={{height:100}}></img>
+          <p>Please Sign In to Continue.</p>
+          <p> Don't have an account? Create one now!</p>
+        </div>
+          <SignInPage
+              signIn={NewAccount}
+              providers = {providers}
+              slots={{
+                title: TitleRegister,
+                subtitle: Subtitle,
+                submitButton : CreateAccount,
+                rememberMe: Subtitle
+              }}
+          />
+      </div>
+      </AppProvider>
+      );
+
+}
+
+function LoginAPI(username, password) {
+  return username != null && password != null;
+}
+
+function RegisterAPI(username, password) {
+  return username != null && password != null;
+}
+
+function CreateAccount(){
+  return (
+    <Button type="submit" onClick={NewAccount}>Create Account</Button>
   );
 }
 
-function LoginAPI(username, password){
-  if ((username == "a@b.com") & (password == 1234)){
-    return true;
-  }
-  return false;
+function Login(){
+  return (
+    <Button type="submit" onClick={SignIn}>Sign In</Button>
+  );
 }
+
+
+function TitleRegister(){
+  return (
+    <h2 className="Title">Sign Up?</h2>
+  )
+}
+
+function TitleLogin(){
+  return (
+    <h2 className= "title">Sign In?</h2>
+  )
+}
+function Subtitle(){
+  return (
+    <></>
+  );
+}
+
+
